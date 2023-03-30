e54a744536cdc5668a3748596ae03597bb09efb2
Jeff Walker <jeff@walkerenvres.com>
2023-03-30 16:14:03 -0400

    mdv5 handler

--- pii-detector/mdv5_handler.py
+++ pii-detector/mdv5_handler.py
@@ -4,11 +4,13 @@ from ts.torch_handler.base_handler import BaseHandler
 import numpy as np
 import base64
 import torch
-import torchvision.transforms as tf
 import torchvision
 import io
 from PIL import Image
 
+from utils.augmentations import letterbox
+from utils.general import non_max_suppression, xyxy2xywh, scale_coords
+import ct_utils
 
 class ModelHandler(BaseHandler):
     """
@@ -16,7 +18,9 @@ class ModelHandler(BaseHandler):
     """
 
     img_size = 640
-    min_conf_thresh = 0.001
+    min_conf_thresh = 0.005
+    stride = 64
+
     """Image size (px). Images will be resized to this resolution before inference.
     """
 
@@ -32,11 +36,6 @@ class ModelHandler(BaseHandler):
             Tensor: single Tensor of shape [BATCH_SIZE, 3, IMG_SIZE, IMG_SIZE]
         """
         images = []
-
-        transform = tf.Compose([
-            tf.ToTensor(),
-            tf.Resize((self.img_size, self.img_size))
-        ])
 
         # load images
         # taken from https://github.com/pytorch/serve/blob/master/ts/torch_handler/vision_handler.py
@@ -59,149 +58,75 @@ class ModelHandler(BaseHandler):
 
             # force convert to tensor
             # and resize to [img_size, img_size]
-            image = transform(image)
 
-            images.append(image)
+            target_size = self.img_size
+            print(f"target_size: {target_size}")
+
+            image = np.asarray(image)
+            self.original_shape = image.shape
+            print(f"original_shape: {self.original_shape}")
+
+            img = letterbox(image, new_shape=target_size,
+                                 stride=self.stride, auto=False)[0]  # JIT requires auto=False
+            #print(f"img.shape: {img.shape}")
+            img = img.transpose((2, 0, 1))  # HWC to CHW; PIL Image is RGB already
+            img = np.ascontiguousarray(img)
+            img = torch.from_numpy(img)
+            # img = img.to(self.device)
+            img = img.float()
+            img /= 255
+            print(f"img.size(): {img.size()}")
+            print(f"img.mean: {img.mean()}")
+
+            images.append(img)
 
         # convert list of equal-size tensors to single stacked tensor
         # has shape BATCH_SIZE x 3 x IMG_SIZE x IMG_SIZE
         images_tensor = torch.stack(images).to(self.device)
+        self.final_shape = images_tensor.numpy().shape
+        print(f"final_shape: {self.final_shape}")
 
         return images_tensor
 
     def postprocess(self, inference_output):
         # perform NMS (nonmax suppression) on model outputs
-        pred = non_max_suppression(inference_output[0], conf_thres=self.min_conf_thresh, iou_thres=.45)
+        pred = non_max_suppression(inference_output[0], conf_thres=self.min_conf_thresh)
 
         # initialize empty list of detections for each image
-        detections = [[] for _ in range(len(pred))]
+        result = {}
+        detections = []
+        max_conf = 0.0
 
-        for i, image_detections in enumerate(pred):  # axis 0: for each image
-            for det in image_detections:  # axis 1: for each detection
-                # x1,y1,x2,y2 in normalized image coordinates (i.e. 0.0-1.0)
-                xyxy = det[:4] / self.img_size
-                # confidence value
-                conf = det[4].item()
-                # index of predicted class
-                class_idx = int(det[5].item())
-                # get label of predicted class
-                # if missing, then just return class idx
-                label = self.mapping.get(str(class_idx), class_idx)
+        CONF_DIGITS = 3
+        COORD_DIGITS = 4
 
-                detections[i].append({
-                    "x1": xyxy[0].item(),
-                    "y1": xyxy[1].item(),
-                    "x2": xyxy[2].item(),
-                    "y2": xyxy[3].item(),
-                    "confidence": conf,
-                    "class": class_idx + 1 # schema we use is 1 for animal, 2 for person, 3 for vehicle
-                })
+        gn = torch.tensor(self.original_shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        # This is a loop over detection batches, which will always be length 1 in our case,
+        # since we're not doing batch inference.
+        for det in pred:
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(self.final_shape[2:], det[:, :4], self.original_shape).round()
 
-        # format each detection
-        return detections
+                for *xyxy, conf, cls in reversed(det):
+                    # normalized center-x, center-y, width and height
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
 
+                    api_box = ct_utils.convert_yolo_to_xywh(xywh)
 
-def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
-                        labels=(), max_det=1000):
-    """Runs Non-Maximum Suppression (NMS) on inference results
-    Returns:
-         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
-    """
+                    conf = ct_utils.truncate_float(conf.tolist(), precision=CONF_DIGITS)
 
-    nc = prediction.shape[2] - 5  # number of classes
-    xc = prediction[..., 4] > conf_thres  # candidates
+                    # MegaDetector output format's categories start at 1, but this model's start at 0
+                    cls = int(cls.tolist()) + 1
+                    if cls not in (1, 2, 3):
+                        raise KeyError(f'{cls} is not a valid class.')
 
-    # Checks
-    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
-    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
-
-    # Settings
-    # (pixels) minimum and maximum box width and height
-    min_wh, max_wh = 2, 4096
-    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
-    time_limit = 10.0  # seconds to quit after
-    redundant = True  # require redundant detections
-    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-    merge = False  # use merge-NMS
-
-    output = [torch.zeros((0, 6), device=prediction.device)
-              ] * prediction.shape[0]
-    for xi, x in enumerate(prediction):  # image index, image inference
-        # Apply constraints
-        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
-        x = x[xc[xi]]  # confidence
-
-        # Cat apriori labels if autolabelling
-        if labels and len(labels[xi]):
-            l = labels[xi]
-            v = torch.zeros((len(l), nc + 5), device=x.device)
-            v[:, :4] = l[:, 1:5]  # box
-            v[:, 4] = 1.0  # conf
-            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
-            x = torch.cat((x, v), 0)
-
-        # If none remain process next image
-        if not x.shape[0]:
-            continue
-
-        # Compute conf
-        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
-
-        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
-        box = xywh2xyxy(x[:, :4])
-
-        # Detections matrix nx6 (xyxy, conf, cls)
-        if multi_label:
-            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
-            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-        else:  # best class only
-            conf, j = x[:, 5:].max(1, keepdim=True)
-            x = torch.cat((box, conf, j.float()), 1)[
-                conf.view(-1) > conf_thres]
-
-        # Filter by class
-        if classes is not None:
-            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
-
-        # Apply finite constraint
-        # if not torch.isfinite(x).all():
-        #     x = x[torch.isfinite(x).all(1)]
-
-        # Check shape
-        n = x.shape[0]  # number of boxes
-        if not n:  # no boxes
-            continue
-        elif n > max_nms:  # excess boxes
-            # sort by confidence
-            x = x[x[:, 4].argsort(descending=True)[:max_nms]]
-
-        # Batched NMS
-        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-        # boxes (offset by class), scores
-        boxes, scores = x[:, :4] + c, x[:, 4]
-        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-        if i.shape[0] > max_det:  # limit detections
-            i = i[:max_det]
-        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-            iou = torchvision.box_iou(
-                boxes[i], boxes) > iou_thres  # iou matrix
-            weights = iou * scores[None]  # box weights
-            x[i, :4] = torch.mm(weights, x[:, :4]).float(
-            ) / weights.sum(1, keepdim=True)  # merged boxes
-            if redundant:
-                i = i[iou.sum(1) > 1]  # require redundancy
-
-        output[xi] = x[i]
-
-    return output
-
-
-def xywh2xyxy(x):
-    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
-    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
-    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
-    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
-    return y
+                    detections.append({
+                        'category': str(cls),
+                        'conf': conf,
+                        'bbox': ct_utils.truncate_float_array(api_box, precision=COORD_DIGITS)
+                    })
+                    max_conf = max(max_conf, conf)
+        result['detections'] = detections
+        result['max_conf'] = max_conf
+        return [result]
